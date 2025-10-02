df95cd5f6b76be9bd330c1e20be6617cfcd2a647
Mohamed Ali <Mohamed.Ali@bravebison.com>
2025-10-02 10:48:31 +0300

    new changes

--- src/components/RichText/index.js
+++ src/components/RichText/index.js
@@ -26,7 +26,7 @@ const getHeadingTag = (heading) => {
 		case 'Paragraph':
 			return 'p'
 		default:
-          	return null;
+			return null;
 	}
 }
 const Heading = ({heading, children, customClasses, defaults, ...props}) => {
@@ -45,17 +45,17 @@ const SmartLink = ({ to, children, ...props }) => {
   const isInternal = /^\/(?!\/)/.test(to) || to.startsWith(window.location.origin)
 
   if (isInternal) {
-    return (
-      <Link to={to} {...props}>
-        {children}<LinkIcon className={classes.linkIcon}/>
-      </Link>
-    )
+	return (
+	  <Link to={to} {...props}>
+		{children}<LinkIcon className={classes.linkIcon}/>
+	  </Link>
+	)
   }
 
   return (
-    <a href={to} target="_blank" rel="noreferrer" {...props}>
-      {children}
-    </a>
+	<a href={to} target="_blank" rel="noreferrer" {...props}>
+	  {children}
+	</a>
   )
 }
 
@@ -83,75 +83,75 @@ const RichText = ({content, ele, useHeadings, slug, relatedArticles, staticImage
 		return urlObj.search;
 	}
 	const options = {
-	  	renderMark: {
-	    	[MARKS.BOLD]: (text) => <strong className={classes.bold}>{text}</strong>,
-	    	[MARKS.ITALIC]: (text) => <em className={classes.italics}>{text}</em>,
-	    	[MARKS.UNDERLINE]: text => <u className={classes.underLines}>{text}</u>,
-		    [MARKS.SUBSCRIPT]: text => <sub className={classes.sub}>{text}</sub>,
-		    [MARKS.SUPERSCRIPT]: text => <sup className={classes.sup}>{text}</sup>,
-		    [MARKS.CODE]: text => <code className={classes.code}>{text}</code>,
-	  	},
-	  	renderNode: {
-	    	[BLOCKS.PARAGRAPH]: (node, children) => <Heading defaults="p" heading={useHeadings} customClasses={classes.paragraph} {...props}>{children}</Heading>,
-	    	[BLOCKS.HEADING_1]: (node, children) => (
-		        <Heading defaults="h1" heading={useHeadings} customClasses={classes.heading_1}>
-		          {children}
-		        </Heading>
-		    ),
-		    [BLOCKS.HEADING_2]: (node, children) => (
-		        <Heading defaults="h2" heading={useHeadings} customClasses={classes.heading_2}>
-		          {children}
-		        </Heading>
-		    ),
-		    [BLOCKS.HEADING_3]: (node, children) => (
-		        <Heading defaults="h3" heading={useHeadings} customClasses={classes.heading_3}>
-		          {children}
-		        </Heading>
-		    ),
-		    [BLOCKS.HEADING_4]: (node, children) => (
-		        <Heading defaults="h4" heading={useHeadings} customClasses={classes.heading_4}>
-		          {children}
-		        </Heading>
-		    ),
-		    [BLOCKS.HEADING_5]: (node, children) => (
-		        <Heading defaults="h5" heading={useHeadings} customClasses={classes.heading_5}>
-		          {children}
-		        </Heading>
-		    ),
-		    [BLOCKS.HEADING_6]: (node, children) => (
-		        <Heading defaults="h6" heading={useHeadings} customClasses={classes.heading_6}>
-		          {children}
-		        </Heading>
-		    ),
-		    [BLOCKS.OL_LIST]: (node, children) => (
-		        <ol className={classes.olList}>{children}</ol>
-		    ),
-		    [BLOCKS.UL_LIST]: (node, children) => (
-		        <ul className={classes.ulList}>{children}</ul>
-		    ),
+		renderMark: {
+			[MARKS.BOLD]: (text) => <strong className={classes.bold}>{text}</strong>,
+			[MARKS.ITALIC]: (text) => <em className={classes.italics}>{text}</em>,
+			[MARKS.UNDERLINE]: text => <u className={classes.underLines}>{text}</u>,
+			[MARKS.SUBSCRIPT]: text => <sub className={classes.sub}>{text}</sub>,
+			[MARKS.SUPERSCRIPT]: text => <sup className={classes.sup}>{text}</sup>,
+			[MARKS.CODE]: text => <code className={classes.code}>{text}</code>,
+		},
+		renderNode: {
+			[BLOCKS.PARAGRAPH]: (node, children) => <Heading defaults="p" heading={useHeadings} customClasses={classes.paragraph} {...props}>{children}</Heading>,
+			[BLOCKS.HEADING_1]: (node, children) => (
+				<Heading defaults="h1" heading={useHeadings} customClasses={classes.heading_1}>
+				  {children}
+				</Heading>
+			),
+			[BLOCKS.HEADING_2]: (node, children) => (
+				<Heading defaults="h2" heading={useHeadings} customClasses={classes.heading_2}>
+				  {children}
+				</Heading>
+			),
+			[BLOCKS.HEADING_3]: (node, children) => (
+				<Heading defaults="h3" heading={useHeadings} customClasses={classes.heading_3}>
+				  {children}
+				</Heading>
+			),
+			[BLOCKS.HEADING_4]: (node, children) => (
+				<Heading defaults="h4" heading={useHeadings} customClasses={classes.heading_4}>
+				  {children}
+				</Heading>
+			),
+			[BLOCKS.HEADING_5]: (node, children) => (
+				<Heading defaults="h5" heading={useHeadings} customClasses={classes.heading_5}>
+				  {children}
+				</Heading>
+			),
+			[BLOCKS.HEADING_6]: (node, children) => (
+				<Heading defaults="h6" heading={useHeadings} customClasses={classes.heading_6}>
+				  {children}
+				</Heading>
+			),
+			[BLOCKS.OL_LIST]: (node, children) => (
+				<ol className={classes.olList}>{children}</ol>
+			),
+			[BLOCKS.UL_LIST]: (node, children) => (
+				<ul className={classes.ulList}>{children}</ul>
+			),
 			[BLOCKS.LIST_ITEM]: (node, children) => (
-		        <li className={classes.li}>{children}</li>
-		    ),
-		    [BLOCKS.TABLE]: (node, children) => {
-		        return (
-		          	<div className={classes.tableWrapper}>
-		            	<table className={classes.table}>
-		              		<tbody>{children}</tbody>
-		            	</table>
-		          	</div>
-		        )
-		    },
-		    [BLOCKS.TABLE_ROW]: (node, children) => <tr className={classes.tableRow}>{children}</tr>,
-		    [BLOCKS.TABLE_HEADER_CELL]: (node, children) => (
-		        <th className={classes.tableHead}>{children}</th>
-		    ),
-		    [BLOCKS.TABLE_CELL]: (node, children) => (
-		        <td className={classes.tableCell}>{children}</td>
-		    ),
-		    [BLOCKS.HR]: () => <hr className={classes.hr}/>,
-		    [BLOCKS.QUOTE]: (node, children) => <blockquote className={classes.blockquote}>
-		    	{children}
-		    </blockquote>,
+				<li className={classes.li}>{children}</li>
+			),
+			[BLOCKS.TABLE]: (node, children) => {
+				return (
+					<div className={classes.tableWrapper}>
+						<table className={classes.table}>
+							<tbody>{children}</tbody>
+						</table>
+					</div>
+				)
+			},
+			[BLOCKS.TABLE_ROW]: (node, children) => <tr className={classes.tableRow}>{children}</tr>,
+			[BLOCKS.TABLE_HEADER_CELL]: (node, children) => (
+				<th className={classes.tableHead}>{children}</th>
+			),
+			[BLOCKS.TABLE_CELL]: (node, children) => (
+				<td className={classes.tableCell}>{children}</td>
+			),
+			[BLOCKS.HR]: () => <hr className={classes.hr}/>,
+			[BLOCKS.QUOTE]: (node, children) => <blockquote className={classes.blockquote}>
+				{children}
+			</blockquote>,
 
 			[BLOCKS.EMBEDDED_ASSET]: (node) => {
 				const { title, gatsbyImageData, width, height } = node.data.target
@@ -170,50 +170,50 @@ const RichText = ({content, ele, useHeadings, slug, relatedArticles, staticImage
 					</>
 				)
 			},
-		    [BLOCKS.EMBEDDED_ENTRY]: node => {
-		        const block = node.data.target
-		        return <ContentReader data={block} />
-		    },
-		    ['embedded-entry-inline']: node => {
-		        const block = node.data.target
-		        return <ContentReader data={block} />
-		    },
-		    [INLINES.HYPERLINK]: (node, children) => (
+			[BLOCKS.EMBEDDED_ENTRY]: node => {
+				const block = node.data.target
+				return <ContentReader data={block} />
+			},
+			['embedded-entry-inline']: node => {
+				const block = node.data.target
+				return <ContentReader data={block} />
+			},
+			[INLINES.HYPERLINK]: (node, children) => (
 				<>
 					{(node.data.uri.includes('?keyword=')) ? 
 						<>{getBackLink(node.data.uri, children)}</>
 						:
 						<SmartLink to={node.data.uri} className={classes.link}>
-							About Page
+							{children}
 						</SmartLink>
 					}
 				</>
-		    ),
-		    [INLINES.ASSET_HYPERLINK]: (node, children) => (
-		        <a href={node.data.target.file.url} className={classes.link}>{children}</a>
-		    ),
-		    [INLINES.ENTRY_HYPERLINK]: (node, children) => {
-		    	const block = node.data.target
-                return <Link to={block.url}>{children}</Link>
-		    },
-	  	},
-	  	renderText: text =>
-      	React.Children.toArray(
-        	text.split('\n').map((t, i) =>
-          		text.length === 0 && t === '' ? '' : i > 0 ? (
-            		<>
+			),
+			[INLINES.ASSET_HYPERLINK]: (node, children) => (
+				<a href={node.data.target.file.url} className={classes.link}>{children}</a>
+			),
+			[INLINES.ENTRY_HYPERLINK]: (node, children) => {
+				const block = node.data.target
+				return <Link to={block.url}>{children}</Link>
+			},
+		},
+		renderText: text =>
+		React.Children.toArray(
+			text.split('\n').map((t, i) =>
+				text.length === 0 && t === '' ? '' : i > 0 ? (
+					<>
 					{useHeadings ?
-              			<span className={classes.br}></span>
+						<span className={classes.br}></span>
 						:
 						<br />
 					}
-              		{t}
-            		</>
-          		) : (
-            		t
-          		)
-        	)
-    	),
+					{t}
+					</>
+				) : (
+					t
+				)
+			)
+		),
 	}
 	return (
 		<div ref={ele} {...props}>
